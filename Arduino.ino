5db6ad41b4a92d101dce1212f8fe9ee909ff4e07
GabiAndi <gabiandiagui@gmail.com>
2021-07-27 16:42:11 -0300

    Fix: Trigger WDT

--- Arduino.ino
+++ Arduino.ino
@@ -1,30 +1,21 @@
 /***************************************** Librerías *****************************************/
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
-
-#include <SPI.h>
-#include <MFRC522.h>
-
-#include <Adafruit_GFX.h>
-#include <Adafruit_PCD8544.h>
 /*********************************************************************************************/
 
 /**************************************** Definiciones ***************************************/
-#define SS_PIN        D4  // Pin SDA
-#define RST_PIN       D3  // Pin RESET
+
 /*********************************************************************************************/
 
 /***************************************** Instancias ****************************************/
 // Conexión a la RED
-MFRC522 mfrc522(SS_PIN, RST_PIN);
-
 const char googleScriptUrl[] = "script.google.com";
 const uint32_t httpsPort = 443;
 const char googleScriptUrlContent[] = "script.googleusercontent.com";
 
 // Configuración de la RED
-const char* ssid = "DEFYMOTION";
-const char* password = "laquequieras";
+const char ssid[] = "TPLINK_24G";
+const char password[] = "BasexB1Au1974*";
 
 IPAddress staticIP(192, 168, 1, 120);         // IP estática
 IPAddress gateway(192, 168, 1, 1);            // Dirección de gateway
@@ -35,7 +26,7 @@ IPAddress secondaryDNS(8, 8, 4, 4);           // Dirección DNS 2
 // Google
 WiFiClientSecure client;
 
-const String GOOGLE_SCRIPT_ID = "AKfycbzP3ESddK27dx7BUvWEo74vXz3uWSFlrDsImJW6v6N7YC5oMz2GxHFs1SSRY2LtyC-4zA";
+const String GOOGLE_SCRIPT_ID = "AKfycbwpNdDrvYaH2NDdEJJYZyEvE_QygqanishV76WQK6NBh8E7p84jxqP1xf8BcHtmW20N";
 const String UNIT_NAME = "headquarter";
 
 String nombre = "";
@@ -47,9 +38,6 @@ String data = ""; // Datos que se enviaran a Google
 
 void handleDataFromGoogle(String data); // Funcion de envio de datos
 String sendData(String params, char* domain); // Funcion que prepara el paquete de datos a Google
-
-// Display
-Adafruit_PCD8544 display = Adafruit_PCD8544(D10, D9, D2, D1, D0);   // Adafruit_PCD8544(SCLK, DIN, DC, CS, RST)
 /*********************************************************************************************/
 
 /************************************ Configuración inicial **********************************/
@@ -58,60 +46,48 @@ void setup()
   // Pines GPIO
   pinMode(LED_BUILTIN, OUTPUT);
 
-  // Display LCD
-  display.begin();
-
-  display.setContrast(60);
-  display.clearDisplay();
-  display.setTextSize(2);
-  display.setTextColor(BLACK);
-  display.setCursor(0, 0);
-
-  display.println("DEFYMOTION");
-
-  display.display();
+  // Puerto serie
+  Serial.begin(9600);
 
   // Configuración de la conexión
   WiFi.mode(WIFI_STA);  // Modo estación
   WiFi.config(staticIP, gateway, subnet, primaryDNS, secondaryDNS); // Configuración de la conexión
   WiFi.begin(ssid, password); // Se conecta a la RED
 
-  // Se espera a que este realizada la conexión
-  while (WiFi.status() != WL_CONNECTED);
+  Serial.println("Conectando");
 
-  // Configuración del RF
-  SPI.begin();  // Inicio del SPI
-  mfrc522.PCD_Init(); // Iniciamos el lector RF
+  // Se espera a que este realizada la conexión
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(1000);  // Este delay SIEMPRE debe estar, de otra manera, se satura de consultas al ESP con WiFi.status()
+  }
+
+  Serial.println("");
+  Serial.println("WiFi conectado");
   
-  delay(10); // Delay para en inicio del RF
+  delay(100); // Delay para en inicio
 }
 /*********************************************************************************************/
 
 /*************************************** Bucle principal *************************************/
 void loop()
 {
-  // Si una nueva tarjeta es detectada
-  if (mfrc522.PICC_IsNewCardPresent())
+  // Test de conexión
+  data = "";
+  
+  for (uint8_t i = 0xA0 ; i < 4 ; i++)
   {
-    //Seleccionamos una tarjeta
-    if (mfrc522.PICC_ReadCardSerial())
-    {
-      // Se convierte los bytes leidos de ID a un string
-      data = "";
-      
-      for (uint8_t i = 0 ; i < 4 ; i++)
-      {
-        data += String(mfrc522.uid.uidByte[i], HEX);
-      }
-      
-      data = sendData("id=" + UNIT_NAME + "&uid=" + data, NULL); // Paquete de datos
-      
-      handleDataFromGoogle(data); // Envio de datos a Google
-      mfrc522.PICC_HaltA(); // Se finaliza la lectura de la tarjeta actual
-    }
+    data += String(i, HEX);
   }
   
-  delay(250); // Delay entre tomas
+  data = sendData("id=" + UNIT_NAME + "&uid=" + data, NULL); // Paquete de datos
+
+  Serial.println("Enviando: " + data);
+  
+  //handleDataFromGoogle(data); // Envio de datos a Google
+  
+  delay(60000); // Delay entre tomas
 }
 /*********************************************************************************************/
 
@@ -130,20 +106,7 @@ void handleDataFromGoogle(String data)
   hora = fechayhora.substring(indFecha + 1, ind2);
   listado = fechayhora.substring(ind2 + 1, fechayhora.length());
 
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.setTextSize(2);
-  display.println(estado);
-  display.println(nombre);
-  display.println(hora);
-  display.display();
-  
-  delay(4000);
-  
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.setTextSize(1);
-  display.println("-->Ingresos<--");
+  Serial.println("-->Ingresos<--");
   
   if (listado.length() > 1)
   {
@@ -156,7 +119,7 @@ void handleDataFromGoogle(String data)
     {
       pos1 = listado.indexOf("-", pos2);
       palabra = listado.substring(pos2, pos1);
-      display.println(palabra);
+      Serial.println(palabra);
       pos2 = pos1 + 1;
       
       if (pos2 >= posfin)
@@ -165,8 +128,6 @@ void handleDataFromGoogle(String data)
       }
     }
   }
-  
-  display.display();
 }
 
 String sendData(String params, char *domain)
